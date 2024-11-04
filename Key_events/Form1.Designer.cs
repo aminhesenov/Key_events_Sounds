4880db6a2d238c094e767c89e43e2d4cb2324d6c
Amin Hasanov <44285676+aminhesenov@users.noreply.github.com>
2024-11-04 11:43:10 +0400

    Key_events

---
+++ Key_events/Form1.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace Key_events
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            label2 = new Label();
+            button1 = new Button();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 12F, FontStyle.Italic, GraphicsUnit.Point, 0);
+            label1.Location = new Point(12, 164);
+            label1.Name = "label1";
+            label1.Size = new Size(0, 21);
+            label1.TabIndex = 0;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold);
+            label2.Location = new Point(312, 37);
+            label2.Name = "label2";
+            label2.Size = new Size(54, 21);
+            label2.TabIndex = 1;
+            label2.Text = "label2";
+            // 
+            // button1
+            // 
+            button1.Location = new Point(368, 298);
+            button1.Name = "button1";
+            button1.Size = new Size(125, 48);
+            button1.TabIndex = 2;
+            button1.Text = "Play";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(button1);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            KeyDown += Form1_KeyDown;
+            KeyPress += Form1_KeyPress;
+            KeyUp += Form1_KeyUp;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private Button button1;
+    }
+}
---
+++ Key_events/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System.Media;
+
+namespace Key_events
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void Form1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            label1.Text += e.KeyChar;
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            label2.Text = "Düyməyə basdınız";
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            label2.Text = "Düyməyə buraxdınız";
+        }
+        SoundPlayer s = new SoundPlayer("D:\\C# dersleri\\img and details\\buttonsound.wav");
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //s.Play();
+        }
+       
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            s.Play();
+        }
+    }
+}
